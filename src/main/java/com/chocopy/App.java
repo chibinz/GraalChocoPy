fa5de7979901c3f67a270c52b9545bf1fccec89a
Chibin Zhang <chibinzhang@hotmail.com>
2022-12-02 14:27:30 +0100

    feat: inherit `ChocoPyLanguage` from `TruffleLanguage`

--- src/main/java/com/chocopy/App.java
+++ src/main/java/com/chocopy/App.java
@@ -1,13 +1,29 @@
 package com.chocopy;
 
+import com.oracle.truffle.api.TruffleLanguage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        var l = new ChocoPyLanguage();
+    }
+}
+
+class ChocoPyContext {
+    public ChocoPyContext(TruffleLanguage.Env env) {
+    }
+}
+
+class ChocoPyLanguage extends TruffleLanguage<ChocoPyContext> {
+    public ChocoPyLanguage() {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public ChocoPyContext createContext(TruffleLanguage.Env env) {
+        return new ChocoPyContext(env);
     }
 }
