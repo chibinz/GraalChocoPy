d69a8f48f74a7531ffabde002e62fd4ce607c086
Chibin Zhang <chibinzhang@hotmail.com>
2022-12-09 20:40:37 +0100

    semantics: reorder evaluation rules to match grammar

--- app/src/main/java/org/chocopy/ChocoPyVisitor.java
+++ app/src/main/java/org/chocopy/ChocoPyVisitor.java
@@ -16,101 +16,16 @@ public class ChocoPyVisitor extends ChocoPyParserBaseVisitor<Object> {
     }
 
     @Override
-    public Object visitVar_def(ChocoPyParser.Var_defContext ctx) {
-        var id = visitTyped_var(ctx.typed_var());
-        var val = visit(ctx.literal());
-
-        memory.put(id, val);
-        System.out.println(memory);
-
-        return NONE;
-    }
-
-    @Override
     public String visitTyped_var(ChocoPyParser.Typed_varContext ctx) {
         return ctx.IDENTIFIER().getText();
     }
 
     @Override
-    public Object visitLiteral(ChocoPyParser.LiteralContext ctx) {
-        var rule = ctx.lit.getType();
+    public Object visitVar_def(ChocoPyParser.Var_defContext ctx) {
+        var id = visitTyped_var(ctx.typed_var());
+        var val = visit(ctx.literal());
 
-        return switch (rule) {
-            case ChocoPyParser.NONE -> NONE;
-            case ChocoPyParser.TRUE -> true;
-            case ChocoPyParser.FALSE -> false;
-            case ChocoPyParser.INTEGER -> Integer.parseInt(ctx.getText());
-            case ChocoPyParser.IDSTRING -> ctx.getText();
-            case ChocoPyParser.STRING -> ctx.getText();
-            default -> throw new RuntimeException("Unknown literal type");
-        };
-    }
-
-    @Override
-    public Object visitIdExpr(ChocoPyParser.IdExprContext ctx) {
-        var id = ctx.IDENTIFIER().getText();
-        var val = memory.get(id);
-
-        if (val == null) {
-            throw new RuntimeException("Undefined variable: " + id);
-        }
-
-        return val;
-    }
-
-    @Override
-    public Object visitListExpr(ChocoPyParser.ListExprContext ctx) {
-        var len = ctx.expr().size();
-        var list = new Object[len];
-
-        for (int i = 0; i < len; i++) {
-            list[i] = visit(ctx.expr(i));
-        }
-
-        return list;
-    }
-
-    @Override
-    public Object visitBinExpr(ChocoPyParser.BinExprContext ctx) {
-        var left = Integer.class.cast(visit(ctx.pexpr(0)));
-        var right = Integer.class.cast(visit(ctx.pexpr(1)));
-        var op = ctx.bin_op().getText();
-
-        return switch (op) {
-            case "+" -> left + right;
-            case "-" -> left - right;
-            case "*" -> left * right;
-            case "//" -> left / right;
-            case "%" -> left % right;
-            default -> throw new RuntimeException("Unknown binary operator");
-        };
-    }
-
-    @Override
-    public Object visitIndexExpr(ChocoPyParser.IndexExprContext ctx) {
-        var list = Object[].class.cast(visit(ctx.pexpr()));
-        var index = Integer.class.cast(visit(ctx.index_op().expr()));
-
-        assert list != null;
-        assert index != null;
-
-        return list[index];
-    }
-
-    @Override
-    public Object visitNegExpr(ChocoPyParser.NegExprContext ctx) {
-        var val = Integer.class.cast(visit(ctx.pexpr()));
-        return -val;
-    }
-
-    @Override
-    public Object visitAssignStmt(ChocoPyParser.AssignStmtContext ctx) {
-        var val = visit(ctx.expr());
-
-        for (var id : ctx.target()) {
-            memory.put(id.getText(), val);
-        }
-
+        memory.put(id, val);
         System.out.println(memory);
 
         return NONE;
@@ -159,5 +74,90 @@ public class ChocoPyVisitor extends ChocoPyParserBaseVisitor<Object> {
         }
 
         return NONE;
+    }
+
+    @Override
+    public Object visitAssignStmt(ChocoPyParser.AssignStmtContext ctx) {
+        var val = visit(ctx.expr());
+
+        for (var id : ctx.target()) {
+            memory.put(id.getText(), val);
+        }
+
+        System.out.println(memory);
+
+        return NONE;
+    }
+
+    @Override
+    public Object visitLiteral(ChocoPyParser.LiteralContext ctx) {
+        var rule = ctx.lit.getType();
+
+        return switch (rule) {
+            case ChocoPyParser.NONE -> NONE;
+            case ChocoPyParser.TRUE -> true;
+            case ChocoPyParser.FALSE -> false;
+            case ChocoPyParser.INTEGER -> Integer.parseInt(ctx.getText());
+            case ChocoPyParser.IDSTRING -> ctx.getText();
+            case ChocoPyParser.STRING -> ctx.getText();
+            default -> throw new RuntimeException("Unknown literal type");
+        };
+    }
+
+    @Override
+    public Object visitIdExpr(ChocoPyParser.IdExprContext ctx) {
+        var id = ctx.IDENTIFIER().getText();
+        var val = memory.get(id);
+
+        if (val == null) {
+            throw new RuntimeException("Undefined variable: " + id);
+        }
+
+        return val;
+    }
+
+    @Override
+    public Object visitListExpr(ChocoPyParser.ListExprContext ctx) {
+        var len = ctx.expr().size();
+        var list = new Object[len];
+
+        for (int i = 0; i < len; i++) {
+            list[i] = visit(ctx.expr(i));
+        }
+
+        return list;
+    }
+
+    @Override
+    public Object visitIndexExpr(ChocoPyParser.IndexExprContext ctx) {
+        var list = Object[].class.cast(visit(ctx.pexpr()));
+        var index = Integer.class.cast(visit(ctx.index_op().expr()));
+
+        assert list != null;
+        assert index != null;
+
+        return list[index];
+    }
+
+    @Override
+    public Object visitBinExpr(ChocoPyParser.BinExprContext ctx) {
+        var left = Integer.class.cast(visit(ctx.pexpr(0)));
+        var right = Integer.class.cast(visit(ctx.pexpr(1)));
+        var op = ctx.bin_op().getText();
+
+        return switch (op) {
+            case "+" -> left + right;
+            case "-" -> left - right;
+            case "*" -> left * right;
+            case "//" -> left / right;
+            case "%" -> left % right;
+            default -> throw new RuntimeException("Unknown binary operator");
+        };
+    }
+
+    @Override
+    public Object visitNegExpr(ChocoPyParser.NegExprContext ctx) {
+        var val = Integer.class.cast(visit(ctx.pexpr()));
+        return -val;
     }
 }
