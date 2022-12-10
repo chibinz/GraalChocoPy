2c849d23e700d960674092d4078753cb24044e9a
Chibin Zhang <chibinzhang@hotmail.com>
2022-12-10 17:03:58 +0100

    truffle: language registration and inheritance boilerplate

--- app/src/main/java/org/chocopy/ChocoPyLanguage.java
+++ app/src/main/java/org/chocopy/ChocoPyLanguage.java
@@ -1,14 +1,30 @@
 package org.chocopy;
 
+import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.TruffleLanguage;
 
+@TruffleLanguage.Registration(
+    id = ChocoPyLanguage.ID,
+    name = ChocoPyLanguage.NAME,
+    defaultMimeType = ChocoPyLanguage.MIME_TYPE,
+    characterMimeTypes = ChocoPyLanguage.MIME_TYPE
+)
 public class ChocoPyLanguage extends TruffleLanguage<ChocoPyContext> {
+    public static final String ID = "chocopy";
+    public static final String NAME = "ChocoPy";
+    public static final String MIME_TYPE = "application/x-chocopy";
+
     public ChocoPyLanguage() {
         System.out.println("Hello World!");
     }
 
     @Override
-    public ChocoPyContext createContext(TruffleLanguage.Env env) {
+    protected ChocoPyContext createContext(TruffleLanguage.Env env) {
         return new ChocoPyContext(env);
+    }
+
+    @Override
+    protected CallTarget parse(TruffleLanguage.ParsingRequest request) throws Exception {
+        return null;
     }
 }
