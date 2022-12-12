cb0ba78458aec8ef754e6f9739355341d4b766fc
Chibin Zhang <chibinzhang@hotmail.com>
2022-12-12 15:49:16 +0100

    test: arithmetic, comparison, logical expressions together

---
+++ app/src/test/java/org/chocopy/nodes/expr/ExprNodeTest.java
@@ -0,0 +1,31 @@
+package org.chocopy.nodes.expr;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExprNodeTest {
+    @Test
+    public void integratedTest() {
+        // -1 + 2 == ((3 * 4) % 5) - 1
+        var prog = LogicalNodeGen.create(
+            CompNodeGen.create(
+                AddNodeGen.create(
+                    NegNodeGen.create(new IntNode(1)),
+                    new IntNode(2)
+                ),
+                SubNodeGen.create(
+                    ModNodeGen.create(
+                        MulNodeGen.create(new IntNode(3), new IntNode(4)),
+                        new IntNode(5)
+                    ),
+                    new IntNode(1)
+                ),
+                "=="
+            ),
+            new BoolNode(true),
+            "and"
+        );
+
+        assertEquals(true, prog.executeGeneric(null));
+    }
+}
