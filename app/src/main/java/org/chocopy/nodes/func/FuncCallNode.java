d68f9630ba7328e9a2f3e9974e1fb59f214c3cd0
Chibin Zhang <chibinzhang@hotmail.com>
2023-01-06 16:25:00 +0100

    truffle: implement `FuncCallNode`, does not allow assignment of functions

--- app/src/main/java/org/chocopy/nodes/func/FuncCallNode.java
+++ app/src/main/java/org/chocopy/nodes/func/FuncCallNode.java
@@ -1,15 +1,29 @@
 package org.chocopy.nodes.func;
 
+import java.util.stream.Stream;
+
+import org.chocopy.ChocoPyContext;
 import org.chocopy.nodes.expr.BaseExprNode;
 
+import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 public class FuncCallNode extends BaseExprNode {
-    public FuncCallNode() {
+    private final String name;
+    @Children
+    private final BaseExprNode[] arguments;
+
+    public FuncCallNode(String name, BaseExprNode[] arguments) {
+        this.name = name;
+        this.arguments = arguments;
     }
 
     @Override
     public Object executeGeneric(VirtualFrame frame) {
-        return null;
+        var context = ChocoPyContext.get(this);
+        var callTarget = (CallTarget) context.globalScope.get(name);
+        var args = Stream.of(arguments).map(arg -> arg.executeGeneric(frame)).toArray();
+
+        return callTarget.call(args);
     }
 }
